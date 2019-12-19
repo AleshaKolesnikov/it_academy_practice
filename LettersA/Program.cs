e832673680fd83f1796213ad17f60b19c99d51e4
igor <gob1in@Mail.ru>
2019-12-19 20:56:36 +0300

    practice 6 done

---
+++ LettersA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LettersA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int counter = 0;
+            int numOfArray = 0;
+            while (numOfArray < word.Length)
+            {
+                if (word[numOfArray] == 'A' || word[numOfArray] == 'a')
+                {
+                    counter++;
+                    numOfArray++;
+                }
+                else
+                {
+                    numOfArray++;
+                }
+            }
+            Console.WriteLine($"Количество букв а = {counter}");
+
+            
+            Console.ReadKey();
+        }
+    }
+}
