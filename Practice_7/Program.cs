e6bddb48c3ee6450de42846f45b327e4aa3c823d
kyposani@outlook.com <kyposani@outlook.com>
2019-12-24 19:17:15 +0300

    Task1 done

---
+++ Practice_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practice_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static void Task1()
+        {
+            //массив всегда будет иметь содержимое по умолчанию
+        }
+        
+    }
+}
